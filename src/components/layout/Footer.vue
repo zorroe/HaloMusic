52afbaf8d389ff5253a77842e5e0064ddf85cdea
zorroe <1318753541@qq.com>
2023-05-06 12:30:18 +0800

    音量调节

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -23,19 +23,28 @@
     <div class="flex justify-center items-center flex-grow">
       进度条和播放按钮区域
     </div>
-    <div class="flex items-center gap-6">
+    <div class="flex items-center gap-4">
       <div class="flex justify-center items-center gap-2">
-        <icon-park :icon="volumnIcon" :size="20"></icon-park>
-        <input type="range" min="0" max="100" :value="volume" step="1" class="range range-xs" /> 
-        <div>{{ volume }}</div>
+        <icon-park :icon="volumnIcon" :size="20" @click="handleClickVolumn"></icon-park>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          :value="playerStore.volume"
+          step="1"
+          @input="handleVolumnChange"
+          class="range range-xs"
+        />
+        <div class="w-10 text-sm text-end">{{ playerStore.volume }}</div>
       </div>
       <icon-park
         :icon="playModeIcon"
         :size="20"
         @click="playerStore.toggleLoop"
+        class="footer-icon"
       ></icon-park>
-      <icon-park :icon="MusicList" :size="20"></icon-park>
-      <icon-park :icon="Up" :size="20"></icon-park>
+      <icon-park class="footer-icon" :icon="MusicList" :size="20"></icon-park>
+      <icon-park class="footer-icon" :icon="Up" :size="20"></icon-park>
     </div>
   </div>
 </template>
@@ -53,18 +62,18 @@ import {
 } from "@icon-park/vue-next";
 import { usePlayerStore } from "@/store";
 import pinia from "@/store/store";
-import { computed, ref } from "vue";
+import { computed } from "vue";
 import { openUrl } from "@/utils/common";
 
 const playerStore = usePlayerStore(pinia);
 const curMusic = computed(() => playerStore.song);
-const volume = ref(40)
+const volume = computed(() => playerStore.volume);
 
 // 音量图标
 const volumnIcon = computed(() => {
-  if (playerStore.volume == 0) {
+  if (volume.value == 0) {
     return VolumeMute;
-  } else if (playerStore.volume <= 60) {
+  } else if (volume.value <= 60) {
     return VolumeSmall;
   } else {
     return VolumeNotice;
@@ -81,6 +90,18 @@ const playModeIcon = computed(() => {
     return ShuffleOne;
   }
 });
+
+const handleClickVolumn = ()=>{
+  if (playerStore.volume == 0) {
+    playerStore.setVolume(60);
+  } else {
+    playerStore.setVolume(0);
+  }
+}
+
+const handleVolumnChange = (event: any) => {
+  playerStore.setVolume(event.target.value);
+};
 </script>
 
 <style lang="scss" scoped>
@@ -88,26 +109,27 @@ const playModeIcon = computed(() => {
   @apply h-16 rounded-b px-8 w-full flex justify-around border-t-2 border-gray-400 items-center;
 }
 
-
 .range-xs {
-  height: 0.5rem/* 16px */;
+  height: 0.5rem /* 16px */;
 }
 .range-xs::-webkit-slider-runnable-track {
-    height: 0.25rem/* 4px */;
-    
+  height: 0.25rem /* 4px */;
 }
 .range-xs::-moz-range-track {
-    height: 0.1rem/* 4px */;
+  height: 0.1rem /* 4px */;
 }
 .range-xs::-webkit-slider-thumb {
-    height: 0.5rem/* 16px */;
-    width: 0.5rem/* 16px */;
-    --filler-offset: 0.1rem/* 6.4px */;
-    
+  height: 0.5rem /* 16px */;
+  width: 0.5rem /* 16px */;
+  --filler-offset: 0.1rem /* 6.4px */;
 }
 .range-xs::-moz-range-thumb {
-    height: 1rem/* 16px */;
-    width: 1rem/* 16px */;
-    --filler-offset: 0.4rem/* 6.4px */;
+  height: 1rem /* 16px */;
+  width: 1rem /* 16px */;
+  --filler-offset: 0.4rem /* 6.4px */;
+}
+
+.footer-icon{
+  @apply p-1 rounded transition-all hover:bg-gray-100 duration-300 hover:scale-110 active:scale-90 
 }
 </style>
