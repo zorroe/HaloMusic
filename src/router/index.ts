6787b17265e73ed6ccb8259bfa0b1822244cf8f9
lixuan <1318753541@qq.com>
2024-01-29 22:26:31 +0800

    首页修改每日推荐样式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,88 +1,98 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import { useUserInfoStore } from "@/store";
-import pinia from "@/store/store";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { useUserInfoStore } from '@/store'
+import pinia from '@/store/store'
+import { nextTick } from 'vue'
 
-const userInfoStore = useUserInfoStore(pinia);
+const userInfoStore = useUserInfoStore(pinia)
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: () => import("@/views/home/index.vue"),
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/home/index.vue'),
   },
   {
-    path: "/explore",
-    name: "explore",
-    component: () => import("@/views/explore/index.vue"),
+    path: '/explore',
+    name: 'explore',
+    component: () => import('@/views/explore/index.vue'),
   },
   {
-    path: "/login",
-    name: "login",
-    component: () => import("@/views/login/index.vue"),
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/index.vue'),
   },
   {
-    path: "/me",
-    name: "me",
-    component: () => import("@/views/me/index.vue"),
+    path: '/me',
+    name: 'me',
+    component: () => import('@/views/me/index.vue'),
   },
   {
     // 传入歌手id
-    path: "/artist/:id",
-    name: "artist",
-    component: () => import("@/views/artist/index.vue"),
+    path: '/artist/:id',
+    name: 'artist',
+    component: () => import('@/views/artist/index.vue'),
   },
   {
     path: '/artistTracks/:id',
     name: 'artistTrack',
-    component: () => import("@/views/artist/tracks.vue")
+    component: () => import('@/views/artist/tracks.vue'),
   },
   {
     // 传入歌单id
-    path: "/playlist/:id",
-    name: "playlist",
-    component: () => import("@/views/playlist/index.vue"),
+    path: '/playlist/:id',
+    name: 'playlist',
+    component: () => import('@/views/playlist/index.vue'),
   },
   {
-    path: "/recommendMusic",
-    name: "recommendMusic",
-    component: () => import("@/views/playlist/recommendMusic.vue"),
+    path: '/recommendMusic',
+    name: 'recommendMusic',
+    component: () => import('@/views/playlist/recommendMusic.vue'),
   },
   {
-    path: "/album/:id",
-    name: "album",
-    component: () => import("@/views/album/index.vue"),
+    path: '/album/:id',
+    name: 'album',
+    component: () => import('@/views/album/index.vue'),
   },
   {
-    path: "/mv/:id",
-    name: "mv",
-    component: () => import("@/views/mv/index.vue"),
+    path: '/mv/:id',
+    name: 'mv',
+    component: () => import('@/views/mv/index.vue'),
   },
   {
-    path: "/curPlayList",
-    name: "curPlayList",
-    component: () => import("@/components/common/CurPlayList.vue"),
+    path: '/curPlayList',
+    name: 'curPlayList',
+    component: () => import('@/components/common/CurPlayList.vue'),
   },
   {
-    path: "/search",
-    name: "search",
-    component: () => import("@/views/search/index.vue"),
+    path: '/search',
+    name: 'search',
+    component: () => import('@/views/search/index.vue'),
   },
-];
+]
 export const router = createRouter({
   history: createWebHashHistory(),
   routes: routes,
-});
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return {
+      top: 0,
+    }
+  },
+})
 
 router.beforeEach((to, from, next) => {
-  if (to.name === "login") {
-    next();
+  if (to.name === 'login') {
+    next()
   } else {
     if (userInfoStore.isLogin) {
-      next();
+      next()
     } else {
-      next({ name: "login" });
+      next({ name: 'login' })
     }
   }
-});
+})
 
-export default router;
+
+export default router
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,125 +1,116 @@
 <template>
-  <div v-show="loaded" class="flex flex-col">
+  <div
+    v-show="loaded"
+    class="flex flex-col">
     <div class="text-lv1">推荐歌单</div>
-    <div class="grid-cols-5 grid place-items-center gap-8">
+    <div class="grid-cols-5 grid place-content-between gap-8 my-8">
       <play-list-cover
-        v-for="item in recommendPlayList.slice(0,10)"
-        :data="item"
-      ></play-list-cover>
-    </div>
-    <div class="text-lv1 flex items-center pr-4">
-      <div>每日推荐</div>
-      <div class="flex flex-grow items-center justify-end gap-4">
-        <icon-park
-          :icon="PlayOne"
-          :size="16"
-          class="rounded-full border-black border-2 btn-animation"
-          @click="handlePlayMultiRecommend"
-        ></icon-park>
-        <div
-          class="ea-link text-xs font-bold"
-          @click="routeTo('/recommendMusic')"
-        >
-          查看全部
-        </div>
-      </div>
-    </div>
-    <div>
-      <!-- 每日推荐歌曲列表的随机5首 -->
-      <music-table :data="recommendSongList.slice(0, 5)"></music-table>
-    </div>
-    <div class="text-lv1">推荐歌手</div>
-    <div class="grid-cols-6 grid place-items-center gap-3">
-      <artist-cover v-for="item in recommendArtist" :data="item"></artist-cover>
-    </div>
-    <div class="text-lv1">新专速递</div>
-    <div class="grid-cols-5 grid place-items-center gap-4">
-      <album-cover v-for="item in albumNewest" :data="item"></album-cover>
-    </div>
-    <div class="text-lv1">排行榜</div>
-    <div class="grid-cols-5 grid place-items-center gap-4">
-      <top-list-cover v-for="item in topList" :data="item"></top-list-cover>
+        v-for="item in recommendPlayList.slice(0, 10)"
+        :data="item"></play-list-cover>
     </div>
     <div class="flex h-48 gap-8">
-      <div class="flex items-end rounded-2xl w-1/2 relative overflow-hidden">
-        <img :src="recommendBg" class="recommend">
-        <div class="ml-4 mb-4 text-7xl w-48 font-bold text-white">
+      <div
+        class="flex items-end rounded-2xl w-1/2 relative overflow-hidden"
+        @click="routeTo('/recommendMusic')">
+        <img
+          :src="recommendBg"
+          class="recommend" />
+        <div id="mask"></div>
+        <div class="ml-4 mb-4 text-7xl w-48 font-bold text-white z-50">
           每日推荐
         </div>
         <div class="flex-1"></div>
-          <icon-park
+        <icon-park
           :icon="PlayOne"
           fill="white"
           theme="filled"
           :size="24"
-          class="mr-4 mb-4 p-2 rounded-full transition-all text-white bg-white bg-opacity-20 hover:bg-opacity-40 active:scale-90"></icon-park>
-        </div>
+          @click="handlePlayMultiRecommend"
+          class="mr-4 mb-4 p-2 z-50 rounded-full transition-all text-white bg-white bg-opacity-20 hover:bg-opacity-40 active:scale-90"></icon-park>
+      </div>
       <person-fm class="w-1/2"></person-fm>
+    </div>
+    <div class="text-lv1">推荐歌手</div>
+    <div class="grid-cols-6 grid place-content-between gap-3">
+      <artist-cover
+        v-for="item in recommendArtist"
+        :data="item"></artist-cover>
+    </div>
+    <div class="text-lv1">新专速递</div>
+    <div class="grid-cols-5 grid place-content-between gap-4">
+      <album-cover
+        v-for="item in albumNewest"
+        :data="item"></album-cover>
+    </div>
+    <div class="text-lv1">排行榜</div>
+    <div class="grid-cols-5 grid place-content-between gap-4">
+      <top-list-cover
+        v-for="item in topList"
+        :data="item"></top-list-cover>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { getNewAlbumApi } from "@/api/album";
-import { getrecommendArtistApi } from "@/api/artist";
-import { getrecommendPlayListApi, getTopListApi } from "@/api/playList";
-import { AlbumBaseInfo } from "@/types/albumRel";
-import { ArtistBaseInfo } from "@/types/artistRel";
-import { PlayListBaseInfo } from "@/types/playListRel";
-import { computed, onMounted, ref } from "vue";
-import { routeTo, saveLikeMusicIds, saveSubAlbumIds } from "@/utils/common";
-import { getRecommendSongsApi } from "@/api/music";
-import { MusicBaseInfo } from "@/types/musicRel";
-import dayjs from "dayjs";
-import { PlayOne } from "@icon-park/vue-next";
-import { usePlayerStore } from "@/store";
-import pinia from "@/store/store";
+import { getNewAlbumApi } from '@/api/album'
+import { getrecommendArtistApi } from '@/api/artist'
+import { getrecommendPlayListApi, getTopListApi } from '@/api/playList'
+import { AlbumBaseInfo } from '@/types/albumRel'
+import { ArtistBaseInfo } from '@/types/artistRel'
+import { PlayListBaseInfo } from '@/types/playListRel'
+import { computed, onMounted, ref } from 'vue'
+import { routeTo, saveLikeMusicIds, saveSubAlbumIds } from '@/utils/common'
+import { getRecommendSongsApi } from '@/api/music'
+import { MusicBaseInfo } from '@/types/musicRel'
+import dayjs from 'dayjs'
+import { PlayOne } from '@icon-park/vue-next'
+import { usePlayerStore } from '@/store'
+import pinia from '@/store/store'
 
-
-const playerStore = usePlayerStore(pinia);
+const playerStore = usePlayerStore(pinia)
 
 defineOptions({
-  name: "home",
-});
+  name: 'home',
+})
 
-const loaded = ref(false);
-const recommendPlayList = ref<PlayListBaseInfo[]>([]);
-const recommendArtist = ref<ArtistBaseInfo[]>([]);
-const albumNewest = ref<AlbumBaseInfo[]>([]);
-const topList = ref<PlayListBaseInfo[]>([]);
-const recommendSongList = ref<MusicBaseInfo[]>([]);
+const loaded = ref(false)
+const recommendPlayList = ref<PlayListBaseInfo[]>([])
+const recommendArtist = ref<ArtistBaseInfo[]>([])
+const albumNewest = ref<AlbumBaseInfo[]>([])
+const topList = ref<PlayListBaseInfo[]>([])
+const recommendSongList = ref<MusicBaseInfo[]>([])
 
-const recommendBg = ref("")
+const recommendBg = ref('')
 
 const getRecommendPlayList = async () => {
-  const {recommend} = await getrecommendPlayListApi();
-  
+  const { recommend } = await getrecommendPlayListApi()
+
   recommend.forEach((item: any) => {
     recommendPlayList.value.push({
       id: item.id,
       name: item.name,
       picUrl: item.picUrl,
       playCount: item.playcount,
-    });
-  });
-};
+    })
+  })
+}
 
 const getrecommendArtist = async () => {
-  const { list } = await getrecommendArtistApi();
+  const { list } = await getrecommendArtistApi()
   // 随机6个
-  list.artists.sort(() => Math.random() - 0.5);
+  list.artists.sort(() => Math.random() - 0.5)
   list.artists.splice(0, 6).forEach((item: any) => {
     recommendArtist.value.push({
       id: item.id,
       name: item.name,
       picUrl: item.picUrl,
-    });
-  });
-};
+    })
+  })
+}
 
 const getAlbumNewest = async () => {
-  const { albums } = await getNewAlbumApi();
-  albums.sort(() => Math.random() - 0.5);
+  const { albums } = await getNewAlbumApi()
+  albums.sort(() => Math.random() - 0.5)
   albums.splice(0, 10).forEach((item: any) => {
     albumNewest.value.push({
       id: item.id,
@@ -129,27 +120,27 @@ const getAlbumNewest = async () => {
         return {
           id: item.id,
           name: item.name,
-          picUrl: "",
-        };
+          picUrl: '',
+        }
       }),
-    });
-  });
-};
+    })
+  })
+}
 
 const getTopList = async () => {
-  const { list } = await getTopListApi();
+  const { list } = await getTopListApi()
   list.splice(0, 5).forEach((item: any) => {
     topList.value.push({
       id: item.id,
       name: item.name,
       picUrl: item.coverImgUrl,
       playCount: item.playCount,
-    });
-  });
-};
+    })
+  })
+}
 
 const getRecommendSongs = async () => {
-  const { data } = await getRecommendSongsApi();
+  const { data } = await getRecommendSongsApi()
   data.dailySongs.forEach((item: any) => {
     recommendSongList.value.push({
       id: item.id,
@@ -159,41 +150,40 @@ const getRecommendSongs = async () => {
         return {
           id: item.id,
           name: item.name,
-          picUrl: "",
-        };
+          picUrl: '',
+        }
       }),
-      duration: dayjs(item.dt).format("mm:ss"),
+      duration: dayjs(item.dt).format('mm:ss'),
       album: {
         id: item.al.id,
         name: item.al.name,
       },
-    });
-  });
+    })
+  })
   recommendBg.value = recommendSongList.value[0].picUrl
-
-};
+}
 
 const handlePlayMultiRecommend = () => {
-  const ids = recommendSongList.value.map((item) => item.id);
-  playerStore.playMulti(ids);
-};
+  const ids = recommendSongList.value.map(item => item.id)
+  playerStore.playMulti(ids)
+}
 
 onMounted(async () => {
-  await getRecommendPlayList();
-  await getrecommendArtist();
-  await getRecommendSongs();
-  getTopList();
-  getAlbumNewest();
-  loaded.value = true;
+  await getRecommendPlayList()
+  await getrecommendArtist()
+  await getRecommendSongs()
+  getTopList()
+  getAlbumNewest()
+  loaded.value = true
   setTimeout(() => {
-    saveLikeMusicIds();
-    saveSubAlbumIds();
-  }, 0);
-});
+    saveLikeMusicIds()
+    saveSubAlbumIds()
+  }, 0)
+})
 </script>
 
 <style scoped>
-.recommend{
+.recommend {
   position: absolute;
   top: 0;
   left: 0;
@@ -201,6 +191,17 @@ onMounted(async () => {
   animation: move 38s infinite;
   animation-direction: alternate;
   z-index: 1;
+}
+
+#mask {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 400px;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 2;
 }
 
 @keyframes move {
@@ -211,5 +212,4 @@ onMounted(async () => {
     transform: translateY(-50%);
   }
 }
-
 </style>
