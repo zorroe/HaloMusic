ad99e09f176ff0f9e326f8352f4d9c01158638b7
lixuan <1318753541@qq.com>
2024-02-16 15:33:08 +0800

    v1.1.0 finish

--- src/views/pan/index.vue
+++ src/views/pan/index.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="px-4 flex flex-col">
     <div
-        v-for="music in musicList"
-        class="p-4 rounded flex hover:bg-gray-100 hover:bg-opacity-60 transition"
-        @dblclick.native="playOne(music)">
+      v-for="music in musicList"
+      class="p-4 rounded flex hover:bg-gray-100 hover:bg-opacity-60 transition"
+      @dblclick.native="playOne(music)">
       <div class="overflow-hidden text-ellipsis whitespace-nowrap">
         {{ music.songName }}
       </div>
       <div class="flex-1"></div>
       <div
-          class="min-w-60 w-60 overflow-hidden text-ellipsis whitespace-nowrap text-base">
+        class="min-w-60 w-60 overflow-hidden text-ellipsis whitespace-nowrap text-base">
         {{ music.artist }}
       </div>
     </div>
     <ea-button
-        @click="loadMore"
-        class="mx-auto"
-        v-show="musicList.length < totalCount"
-        type="info">
+      @click="loadMore"
+      class="mx-auto"
+      v-show="musicList.length < totalCount"
+      type="info">
       <div class="text-lg font-bold">加载更多</div>
     </ea-button>
   </div>
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
-import {userCloudApi, userCloudDetailApi} from '@/api/music'
+import { onMounted, ref } from 'vue'
+import { userCloudApi, userCloudDetailApi } from '@/api/music'
 
 const totalCount = ref(0)
 
@@ -37,7 +37,7 @@ const params = ref({
 const musicList = ref<any[]>([])
 
 const userCloud = async () => {
-  const {data, count} = await userCloudApi(params.value)
+  const { data, count } = (await userCloudApi(params.value)) as any
   totalCount.value = count
   musicList.value = [...musicList.value, ...data]
 }
@@ -51,7 +51,7 @@ const loadMore = async () => {
 }
 
 const playOne = async (music: any) => {
-  const res = await userCloudDetailApi({id: music.songId})
+  const res = await userCloudDetailApi({ id: music.songId })
   console.log(res)
 }
 
